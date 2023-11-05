44ed2d3d9b9b4bf9f7f2e38fbc70e42bfa0ac337
Margaret Alade <alade.margaret@yandex.ru>
2023-11-05 16:28:04 +0300

    Initial commit

--- Task-8.1/main.cpp
+++ Task-8.1/main.cpp
@@ -6,9 +6,39 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class bad_length : public std::exception {
+public:
+const char* what() const noexcept override { return "Вы ввели слово запретной длины! До свидания";}
+};
+
+
+int function(std::string str, int forbidden_length) {
+    int length = static_cast<int>(str.length());
+    if (str.length() == forbidden_length) {
+        throw bad_length();
+    }
+    return length;
+}
+
+int main() {
+    std::string user_str;
+    int forbidden_length;
+    do {
+      std::cout << "Введите запретную длину: ";
+      std::cin >> forbidden_length;
+      std::cout << '\n';
+      std::cout << "Введите слово: ";
+      std::cin >> user_str;
+      std::cout << '\n';
+      try {
+          std::cout << "Длина слова \"" << user_str << "\" равна " << function(user_str,forbidden_length) << '\n';
+      } catch (const bad_length& e) {
+        std::cout << e.what() << '\n';
+      }
+  } while (user_str.length() != forbidden_length);
+
+
     return 0;
 }
